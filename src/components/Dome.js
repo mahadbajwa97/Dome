dbd9b96ca94564d84eb1784f39181f01f95e5236
mahadbajwa97 <mahadbajwa97@gmail.com>
2021-11-15 19:29:04 +0500

    dimensions set

--- src/components/Dome.js
+++ src/components/Dome.js
@@ -24,7 +24,7 @@ class Dome extends Component {
         console.log( monsterRight)
     }
     componentDidMount() {
-        setInterval(()=>{this.state.monsterImage==monsterLeft?this.setState({monsterImage:monsterRight}):this.setState({monsterImage:monsterLeft})},3000)
+        setInterval(()=>{this.state.monsterImage===monsterLeft?this.setState({monsterImage:monsterRight}):this.setState({monsterImage:monsterLeft})},3000)
     }
     activateTab(e){
         const id = e.target.name;
@@ -67,47 +67,46 @@ class Dome extends Component {
                     <img src={light} className="light1" alt=""/>
                     <img src={light} className="light2" alt=""/>
                         
-                        <div className="innermost-Dome">
-                        <img name="stats" src={stats} onClick={(e)=>this.activateTab(e)} alt=""/>  
-                        <img name="chicken" src={chicken} onClick={(e)=>this.activateTab(e)} alt=""/>
-                        <img name="weights" src={weights} onClick={(e)=>this.activateTab(e)} alt=""/>
-                        <img name="swords" src={swords} onClick={(e)=>this.activateTab(e)} alt=""/>
-                        <img name="firstAid" src={firstAid} onClick={(e)=>this.activateTab(e)} alt=""/>
-                        <img name="shit" src={shit} onClick={(e)=>this.activateTab(e)} alt=""/>
+                        <div className="innermost-Dome row">
+                        <img name="stats" className="col" src={stats} onClick={(e)=>this.activateTab(e)} alt=""/>  
+                        <img name="chicken" className="col" src={chicken} onClick={(e)=>this.activateTab(e)} alt=""/>
+                        <img name="weights" className="col" src={weights} onClick={(e)=>this.activateTab(e)} alt=""/>
+                        <img name="swords" className="col" src={swords} onClick={(e)=>this.activateTab(e)} alt=""/>
+                        <img name="firstAid" className="col" src={firstAid} onClick={(e)=>this.activateTab(e)} alt=""/>
+                        <img name="shit" className="col" src={shit} onClick={(e)=>this.activateTab(e)} alt=""/>
                        <div className="playBoard">
                            
                            {this.state.tab==="Connecting"?<p>CONNECT</p>:""}
-                           {this.state.tab==="stats"?<div>
-                      <table>
-                          <tr>
-                              <td>AGE</td>
-                              <td>1</td>
-                          </tr>
-                          <tr>
-                              <td>BIRTHSTONE</td>
-                              <td>RUBY</td>
-                          </tr>
-                          <tr>
-                          <td>BREED</td>
-                          <td>123</td> 
-                          </tr>
-                          <tr>
-                              <td>TYPE</td>
-                              <td>4X</td>
-                          </tr>
-                      </table>
-            </div>:""}
+                           {this.state.tab==="stats"? <div className="stats">
+                          <div className="row">
+                              <div className="col">AGE</div>
+                              <div className="col">1</div>
+                          </div>
+                          <div className="row">
+                              <div className="col">BIRTHSTONE</div>
+                              <div className="col">RUBY</div>
+                          </div>
+                          <div className="row">
+                          <div className="col">BREED</div>
+                          <div className="col">123</div> 
+                          </div>
+                          <div className="row">
+                              <div className="col">TYPE</div>
+                              <div className="col">4X</div>
+                          </div></div>:""}
 
-                 {this.state.tab==="monsterButton"?<img src={this.state.monsterImage} className="Monster"/>:""}      
+                 {this.state.tab==="monsterButton"?<div className="Monster"><img src={this.state.monsterImage} /></div>:""}      
                        </div>
                        
                         </div>  
-                    
-                    <div className="scoreCard">
-                        {this.state.tab==="Connecting"?<center>CONNECT</center>:"4350"}
+                   <div className="row options"> 
+                   
+                    <div className="scoreCard col-2">
+                        {this.state.tab==="Connecting"?<span>CONNECT</span>:"4350"}
                     </div>
-                    <img src={button} className="button" alt="" onClick={(e)=>this.activateTab(e)} name="monsterButton"/>
-                    
+                    <div className="col bar"><i class="fas fa-bars"></i></div>
+                    <div className="col"><img src={button} className="button" alt="" onClick={(e)=>this.activateTab(e)} name="monsterButton"/></div>
+                    </div>
                 </div>
            </div>
        </div>
